1ba79d9db5e9e603f15def21436606bf23810537
BossLogic314 <bhagavatulaanish@gmail.com>
2022-10-21 11:51:57 +0530

    Sending queues established

---
+++ src/PlexShare/networking/Queues/IQueue.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Networking.Queues
+{
+    public interface IQueue
+    {
+        public int Size(bool isHighPriority);
+        public bool IsEmpty(bool isHighPriority);
+        public void Clear();
+        public void Enqueue(Packet packet, bool isHighPriority);
+        public Packet Dequeue(bool isHighPriority);
+        public Packet Peek(bool isHighPriority);
+    }
+}
