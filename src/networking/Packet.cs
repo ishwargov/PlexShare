ac9cb5cd6ecc10ec74c2c23fcc486990435b8450
BossLogic314 <bhagavatulaanish@gmail.com>
2022-10-21 20:41:08 +0530

    Moved the networking folder into src

---
+++ src/networking/Packet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlexShareNetworking
+{
+    public class Packet
+    {
+        private string SerializedData;
+        private string ModuleIdentifier;
+
+        // Getters
+        public string getSerializedData()
+        {
+            return SerializedData;
+        }
+
+        public string getModuleIdentifier()
+        {
+            return ModuleIdentifier;
+        }
+
+        // Setters
+        public void setSerializedData(string SerializedData)
+        {
+            this.SerializedData = SerializedData;
+        }
+
+        public void setModuleIdentifier(string ModuleIdentifier)
+        {
+            this.ModuleIdentifier = ModuleIdentifier;
+        }
+    }
+}
---
+++ src/networking/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PlexShareNetworking
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            PlexShareNetworking.Serialization.Serializer s = new PlexShareNetworking.Serialization.Serializer();
+            var message = "Hello";
+
+            String str = s.Serialize(message);
+
+            Console.Out.WriteLine(str);
+            Console.Out.WriteLine(s.Deserialize(str));
+        }
+    }
+}
