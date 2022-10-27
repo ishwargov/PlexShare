b3cc4185c1fe2d9469d3a97b7433e4d1ef0c5aa6
Neel Kabra <kabraneel@gmail.com>
2022-10-28 00:57:45 +0530

    Pushing Basic Changes

---
+++ src/PlexShareApp/AuthenticationView.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PlexShareApp
+{
+    /// <summary>
+    /// Interaction logic for AuthenticationView.xaml
+    /// </summary>
+    public partial class AuthenticationView : Window
+    {
+        public AuthenticationView()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            _NavigationFrame.Navigate(new HomePageView());
+        }
+
+
+    }
+}
