4eeada81bcc9ec1652e22ca501e09d7a3866952a
Narvik <narvik.vesangi@gmail.com>
2022-10-31 01:23:11 +0530

    Implemented IContentClient
    Added requried data models and enums for IContentClient interface implementation

--- src/PlexShareContent/Client/IContentClient.cs
+++ src/PlexShareContent/Client/IContentClient.cs
@@ -11,12 +11,22 @@
  *				 to notification from this class. 
  *****************************************************************************/
 
-using System;
+using PlexShareContent.DataModels;
 
-namespace PlexShareContent
+namespace PlexShareContent.Client
 {
     public interface IContentClient
     {
-        // implement content client interface.
+        /// <summary>
+        /// Sends chat or file data to clients
+        /// </summary>
+        /// <param name="contentData">Data to be sent</param>
+        public void ClientSendData(SendContentData contentData);
+
+        /// <summary>
+        /// Lets client subscribe to notifications from this class
+        /// </summary>
+        /// <param name="subscriber">Subscriber object which is an implementation of the IContentListener interface</param>
+        public void ClientSubscribe(IContentListener subscriber);
     }
 }
---
+++ src/PlexShareContent/DataModels/SendContentData.cs
@@ -0,0 +1,43 @@
+﻿/******************************************************************************
+ * Filename    = SendContentData.cs
+ *
+ * Author      = Narvik Nandan
+ *
+ * Product     = PlexShare
+ * 
+ * Project     = PlexShareContent
+ *
+ * Description = Class containing metadata related to a message. Used for 
+ *               send data. 
+ *****************************************************************************/
+
+namespace PlexShareContent.DataModels
+{
+    public class SendContentData
+    {
+        /// <summary>
+        /// Type of message - chat or file
+        /// </summary>
+        public MessageType Type;
+
+        /// <summary>
+        /// Content of message if chat type message.
+        /// File path if file type message.
+        /// </summary>
+        public string Data;
+
+        /// <summary>
+        /// List containing the receiver IDs.
+        /// </summary>
+        public int[]? ReceiverIDs;
+
+        /// <summary>
+        /// Constrcutor to initialize the fields
+        /// </summary>
+        public SendContentData()
+        {
+            Data = "";
+            ReceiverIDs = null;
+        }
+    }
+}
