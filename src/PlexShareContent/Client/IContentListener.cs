c24a13d9f50662206006f20626b5207036f337ba
Narvik <narvik.vesangi@gmail.com>
2022-10-31 01:23:44 +0530

    Implemented IContentListener
    Added requried data model and implemented IContentListener interface

---
+++ src/PlexShareContent/Client/IContentListener.cs
@@ -0,0 +1,25 @@
+﻿/******************************************************************************
+ * Filename    = IContentListener.cs
+ *
+ * Author      = Narvik Nandan
+ *
+ * Product     = PlexShare
+ * 
+ * Project     = PlexShareContent
+ *
+ * Description = Interface to notify clients when a message has been received.
+ *****************************************************************************/
+
+using PlexShareContent.DataModels;
+
+namespace PlexShareContent.Client
+{
+    public interface IContentListener
+    {
+        /// <summary>
+        /// Handles the reception of a message.
+        /// </summary>
+        /// <param name="contentData"></param>
+        void OnMessageReceived(ReceiveContentData contentData);
+    }
+}
