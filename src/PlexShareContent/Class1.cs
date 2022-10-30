b4f673d841e200da28a579681c64331010b33618
Narvik <narvik.vesangi@gmail.com>
2022-10-30 22:41:04 +0530

    Initial commit
    Added IContentClient interface file and removed extra class file.

--- src/PlexShareContent/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace PlexShareContent
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/PlexShareContent/Client/IContentClient.cs
@@ -0,0 +1,22 @@
+﻿/******************************************************************************
+ * Filename    = IContentClient.cs
+ *
+ * Author      = Narvik Nandan
+ *
+ * Product     = PlexShare
+ * 
+ * Project     = PlexShareContent
+ *
+ * Description = Interface to let clients send data and subscribe to
+ *				 to notification from this class. 
+ *****************************************************************************/
+
+using System;
+
+namespace PlexShareContent
+{
+    public interface IContentClient
+    {
+        // implement content client interface.
+    }
+}
