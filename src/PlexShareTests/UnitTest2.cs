449d1996808f13307b3b71c158410064e522ee2b
Narvik <narvik.vesangi@gmail.com>
2022-11-01 22:11:08 +0530

    Removed Unit tests

---
+++ src/PlexShareTests/UnitTest2.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace PlexShareTests
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
