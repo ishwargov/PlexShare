d18cd0908d6ad8db5b664d580cdd126974322601
Satyam Mishra <satyammishra37953@gmail.com>
2022-11-02 15:48:57 +0530

    Some Changes

--- src/PlexShareScreenshare/Client/ScreenProcessor.cs
+++ src/PlexShareScreenshare/Client/ScreenProcessor.cs
@@ -8,49 +8,48 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
-// This datatype is for storing a list of the coordinate
-// of pixel and its RGB values i.e. list of ((x, y), (R, G, B))
-using ImageDiffList = System.Collections.Generic.List<System.Tuple<System.Tuple<int, int>, 
-						System.Tuple<int, int, int>>>;
-
-// Each frame consists of the resolution of the image and the ImageDiffList
-using Frame = System.Tuple<System.Tuple<int, int>,
-						System.Collections.Generic.List<System.Tuple<System.Tuple<int, int>, 
-						System.Tuple<int, int, int>>>>;
 using System.Threading;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
-using PlexShareScreenshare.Client;
 
-namespace ScreenShare.Client
+// This datatype is for storing a list of the coordinate
+// of pixel and its RGB values i.e. list of ((x, y), (R, G, B))
+using ImageDiffList = System.Collections.Generic.List<System.Tuple<System.Tuple<int, int>,
+                        System.Tuple<int, int, int>>>;
+
+// Each frame consists of the resolution of the image and the ImageDiffList
+using Frame = System.Tuple<System.Tuple<int, int>,
+                        System.Collections.Generic.List<System.Tuple<System.Tuple<int, int>,
+                        System.Tuple<int, int, int>>>>;
+
+namespace PlexShareScreenshare.Client
 {
-	/// <summary>
-	/// Class contains implementation of the screen processing using threads (tasks)
-	/// </summary>
-	internal class ScreenProcessor
-	{
-		// The queue in which the image will be enqueued after
-		// processing it
-		private Queue<Frame> ProcessedFrame;
+    /// <summary>
+    /// Class contains implementation of the screen processing using threads (tasks)
+    /// </summary>
+    internal class ScreenProcessor
+    {
+        // The queue in which the image will be enqueued after
+        // processing it
+        private Queue<Frame> ProcessedFrame;
 
-		// Processing task
-		private Task ProcessorTask;
+        // Processing task
+        private Task ProcessorTask;
 
-		// The screen capturer object
-		private ScreenCapturer Capturer;
+        // The screen capturer object
+        private ScreenCapturer Capturer;
 
-		// Old and the new resolutions 
-		private Tuple<int, int> OldRes { get; set; }
-		public Tuple<int, int> NewRes { private get; set; }
+        // Old and the new resolutions 
+        private Tuple<int, int> OldRes { get; set; }
+        public Tuple<int, int> NewRes { private get; set; }
 
-		// tokens added to be able to stop the thread execution
-		CancellationTokenSource tokenSource;
-		CancellationToken token;
+        // Tokens added to be able to stop the thread execution
+        CancellationTokenSource tokenSource;
+        CancellationToken token;
 
-		// storing the previous frame
-		Bitmap prevImage;
+        // Storing the previous frame
+        Bitmap prevImage;
 
         /// <summary>
         /// Called by ScreenShareClient
@@ -58,101 +57,112 @@ namespace ScreenShare.Client
         /// cancellation token and the previous image
         /// </summary>
         ScreenProcessor(ScreenCapturer Capturer)
-		{
-			this.Capturer = Capturer;
-			ProcessedFrame = new Queue<Frame>();
-			OldRes = new Tuple<int, int>(720, 1280);
-			NewRes = new Tuple<int, int>(720, 1280);
-			tokenSource = new CancellationTokenSource();
-			token = tokenSource.Token;
-			prevImage = new Bitmap(720, 1280);
+        {
+            this.Capturer = Capturer;
+            ProcessedFrame = new Queue<Frame>();
+            OldRes = new Tuple<int, int>(720, 1280);
+            NewRes = new Tuple<int, int>(720, 1280);
+            tokenSource = new CancellationTokenSource();
+            token = tokenSource.Token;
+            prevImage = new Bitmap(720, 1280);
         }
 
         /// <summary>
         /// Pops and return the image from the queue
         /// </summary>
-        public Frame GetImage() 
-		{
-			while (ProcessedFrame.Count != 0) Thread.Sleep(100);
-			lock(ProcessedFrame)
-			{
+        public Frame GetImage()
+        {
+            while (ProcessedFrame.Count != 0) Thread.Sleep(100);
+            lock (ProcessedFrame)
+            {
                 return ProcessedFrame.Dequeue();
             }
-		}
-		/// <summary>
-		/// In this function we go through every pixel of both the images and
-		/// returns list of those pixels which are different in both the images
-		/// </summary>
-		private ImageDiffList ProcessUsingLockbits(Bitmap processedBitmap, Bitmap processedBitmap1)
-		{
-			ImageDiffList tmp = new ImageDiffList();
-			int count = 0;
-			BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
+        }
+        /// <summary>
+        /// In this function we go through every pixel of both the images and
+        /// returns list of those pixels which are different in both the images
+        /// </summary>
+        private ImageDiffList ProcessUsingLockbits(Bitmap processedBitmap, Bitmap processedBitmap1)
+        {
+            // List for storing the difference in pixels
+            ImageDiffList tmp = new();
+            int count = 0;
+            // Getting BitmapData from the Bitmap of first image
+            // by locking the bits
+            BitmapData bitmapData = processedBitmap.LockBits(new Rectangle(0, 0, processedBitmap.Width, processedBitmap.Height), ImageLockMode.ReadWrite, processedBitmap.PixelFormat);
 
-			int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
-			int byteCount = bitmapData.Stride * processedBitmap.Height;
-			byte[] pixels = new byte[byteCount];
-			IntPtr ptrFirstPixel = bitmapData.Scan0;
-			Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
-			int heightInPixels = bitmapData.Height;
-			int widthInBytes = bitmapData.Width * bytesPerPixel;
-			processedBitmap.UnlockBits(bitmapData);
+            // Flattening of image into an array
+            int bytesPerPixel = Bitmap.GetPixelFormatSize(processedBitmap.PixelFormat) / 8;
+            int byteCount = bitmapData.Stride * processedBitmap.Height;
+            byte[] pixels = new byte[byteCount];
+            IntPtr ptrFirstPixel = bitmapData.Scan0;
+            Marshal.Copy(ptrFirstPixel, pixels, 0, pixels.Length);
+            int heightInPixels = bitmapData.Height;
+            int widthInBytes = bitmapData.Width * bytesPerPixel;
+            processedBitmap.UnlockBits(bitmapData);
 
-			BitmapData bitmapData1 = processedBitmap1.LockBits(new Rectangle(0, 0, processedBitmap1.Width, processedBitmap1.Height), ImageLockMode.ReadWrite, processedBitmap1.PixelFormat);
+            // Getting BitmapData from the Bitmap of second image
+            BitmapData bitmapData1 = processedBitmap1.LockBits(new Rectangle(0, 0, processedBitmap1.Width, processedBitmap1.Height), ImageLockMode.ReadWrite, processedBitmap1.PixelFormat);
 
-			int bytesPerPixel1 = Bitmap.GetPixelFormatSize(processedBitmap1.PixelFormat) / 8;
-			int byteCount1 = bitmapData1.Stride * processedBitmap1.Height;
-			byte[] pixels1 = new byte[byteCount1];
-			IntPtr ptrFirstPixel1 = bitmapData1.Scan0;
-			Marshal.Copy(ptrFirstPixel1, pixels1, 0, pixels1.Length);
-			int heightInPixels1 = bitmapData1.Height;
-			int widthInBytes1 = bitmapData1.Width * bytesPerPixel1;
+            // Flattening of image into an array
+            int bytesPerPixel1 = Bitmap.GetPixelFormatSize(processedBitmap1.PixelFormat) / 8;
+            int byteCount1 = bitmapData1.Stride * processedBitmap1.Height;
+            byte[] pixels1 = new byte[byteCount1];
+            IntPtr ptrFirstPixel1 = bitmapData1.Scan0;
+            Marshal.Copy(ptrFirstPixel1, pixels1, 0, pixels1.Length);
+            int heightInPixels1 = bitmapData1.Height;
+            int widthInBytes1 = bitmapData1.Width * bytesPerPixel1;
+            processedBitmap1.UnlockBits(bitmapData1);
 
-			processedBitmap1.UnlockBits(bitmapData1);
+            // Now iterating over the image array and checking the difference 
+            // in pixel values
+            for (int y = 0; y < heightInPixels; y++)
+            {
+                int currentLine = y * bitmapData.Stride;
+                int currentLine1 = y * bitmapData1.Stride;
+                for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
+                {
+                    // getting the color values from the two images
+                    int oldBlue = pixels[currentLine + x];
+                    int oldGreen = pixels[currentLine + x + 1];
+                    int oldRed = pixels[currentLine + x + 2];
 
-			for (int y = 0; y < heightInPixels; y++)
-			{
-				int currentLine = y * bitmapData.Stride;
-				int currentLine1 = y * bitmapData1.Stride;
-				for (int x = 0; x < widthInBytes; x = x + bytesPerPixel)
-				{
-					int oldBlue = pixels[currentLine + x];
-					int oldGreen = pixels[currentLine + x + 1];
-					int oldRed = pixels[currentLine + x + 2];
+                    int newBlue = pixels1[currentLine1 + x];
+                    int newGreen = pixels1[currentLine1 + x + 1];
+                    int newRed = pixels1[currentLine1 + x + 2];
 
-					int newBlue = pixels1[currentLine1 + x];
-					int newGreen = pixels1[currentLine1 + x + 1];
-					int newRed = pixels1[currentLine1 + x + 2];
-
-					if (oldBlue != newBlue || oldGreen != newGreen || oldRed != newRed)
-					{
-						Tuple<int, int> coordinates = new Tuple<int, int>(x / bytesPerPixel, y);
-						Tuple<int, int, int> colors = new Tuple<int, int, int>(newRed, newGreen, newBlue);
-						tmp.Add(new Tuple<Tuple<int, int>, Tuple<int, int, int>>(coordinates, colors));
-						count++;
-					}
-				}
-			}
-			return tmp;
-		}
-		/// <summary>
-		/// main function which will run in loop and capture the image
-		/// calculate the image bits differences and 
-		/// </summary>
-		private void Processing()
-		{
-			while (true) 
-			{
-				Bitmap img = Capturer.GetImage();
+                    // now if anyone of them is different then start save this pixel 
+                    // coordinates and the RGB value of the second image
+                    if (oldBlue != newBlue || oldGreen != newGreen || oldRed != newRed)
+                    {
+                        Tuple<int, int> coordinates = new Tuple<int, int>(x / bytesPerPixel, y);
+                        Tuple<int, int, int> colors = new Tuple<int, int, int>(newRed, newGreen, newBlue);
+                        tmp.Add(new Tuple<Tuple<int, int>, Tuple<int, int, int>>(coordinates, colors));
+                        count++;
+                    }
+                }
+            }
+            // returning these pixel details
+            return tmp;
+        }
+        /// <summary>
+        /// main function which will run in loop and capture the image
+        /// calculate the image bits differences and 
+        /// </summary>
+        private void Processing()
+        {
+            while (true)
+            {
+                Bitmap img = Capturer.GetImage();
                 img = Compress(img);
                 ImageDiffList DiffList = ProcessUsingLockbits(prevImage, img);
-				lock (ProcessedFrame)
-				{
+                lock (ProcessedFrame)
+                {
                     ProcessedFrame.Append(new Frame(NewRes, DiffList));
                 }
                 prevImage = img;
-			}
-		}
+            }
+        }
 
         /// <summary>
         /// Called by ScreenShareClient when the client starts screen sharing
@@ -160,10 +170,10 @@ namespace ScreenShare.Client
         /// Create the task for the lambda function 
         /// </summary>
         public void StartProcessing()
-		{
-            ProcessorTask = new Task(Processing);
-			ProcessorTask.Start();
-            ProcessorTask.WaitAsync(token);
+        {
+            ProcessorTask = new Task(Processing, token);
+            ProcessorTask.Start();
+
         }
 
         /// <summary>
@@ -172,7 +182,7 @@ namespace ScreenShare.Client
         /// Empty the queue
         /// </summary>
         public void SuspendProcessing()
-		{
+        {
             StopProcessing();
         }
 
@@ -181,9 +191,9 @@ namespace ScreenShare.Client
         /// Resume the thread
         /// </summary>
         public void ResumeProcessing()
-		{
-			StartProcessing();
-		}
+        {
+            StartProcessing();
+        }
 
         /// <summary>
         /// Called by ScreenShareClient when the client stops screen sharing
@@ -191,24 +201,24 @@ namespace ScreenShare.Client
         /// Empty the Queue
         /// </summary>
         public void StopProcessing()
-		{
+        {
             tokenSource.Cancel();
+            ProcessedFrame.Clear();
         }
 
         /// <summary>
         /// Called by StartProcessing
         /// run the compression algorithm and returns list of changes in pixels
-        /// Does it need to be public?
         /// </summary>
         public Bitmap Compress(Bitmap img)
-		{
-			if (NewRes != OldRes)
-			{
+        {
+            if (NewRes != OldRes)
+            {
                 prevImage = new Bitmap(NewRes.Item1, NewRes.Item2);
-				OldRes = NewRes;
+                OldRes = NewRes;
             }
-			img = new Bitmap(img, NewRes.Item1, NewRes.Item2);
-			return img;
-		}
-	}
+            img = new Bitmap(img, NewRes.Item1, NewRes.Item2);
+            return img;
+        }
+    }
 }
