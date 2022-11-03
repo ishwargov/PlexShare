ca60bd70d37258ad1c9dfceb3af600695ec31300
Narvik <66057572+Narvik09@users.noreply.github.com>
2022-11-03 23:58:50 +0530

    Added content tests

---
+++ src/PlexShareTests/ContentTests/Client/ContentClientFactoryTests.cs
@@ -0,0 +1,40 @@
+﻿/******************************************************************************
+ * Filename    = ContentClientFactoryTests.cs
+ *
+ * Author      = Narvik Nandan
+ *
+ * Product     = PlexShare
+ * 
+ * Project     = PlexShareTests
+ *
+ * Description = Tests for content client factory. 
+ *****************************************************************************/
+
+using PlexShareContent.Client;
+
+namespace PlexShareTests.ContentTests.Client
+{
+    public class ContentClientFactoryTests
+    {
+        [Fact]
+        public void GetInstance_SingleThread_ReturnsSingleInstance()
+        {
+            var ref1 = ContentClientFactory.GetInstance();
+            var ref2 = ContentClientFactory.GetInstance();
+
+            Assert.Same(ref1, ref2);
+        }
+        [Fact]
+        public void GetInstance_MultiThread_ReturnsSingleInstance()
+        {
+            IContentClient? ref1 = null;
+            IContentClient? ref2 = null;
+            var t1 = Task.Run(() => { ref1 = ContentClientFactory.GetInstance(); });
+            var t2 = Task.Run(() => { ref2 = ContentClientFactory.GetInstance(); });
+
+            Task.WaitAll(t1, t2);
+
+            Assert.Same(ref1, ref2);
+        }
+    }
+}
