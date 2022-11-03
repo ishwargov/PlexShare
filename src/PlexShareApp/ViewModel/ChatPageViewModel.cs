1bda02c204efb0254fb38571b7f69bb71116bd1c
S, Sughandhan (T DAI ADM-IN) <sughandhan.s@siemens.com>
2022-11-03 22:40:23 +0530

    Added Message Datatype Class

---
+++ src/PlexShareApp/ViewModel/ChatPageViewModel.cs
@@ -0,0 +1,18 @@
+﻿/// <author> Sughandhan S </author>
+/// <created> 03/11/2022 </created>
+/// <summary>
+///     The following is the ViewModel for our ChatPage.
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlexShareApp.ViewModel
+{
+    internal class ChatPageViewModel
+    {
+    }
+}
