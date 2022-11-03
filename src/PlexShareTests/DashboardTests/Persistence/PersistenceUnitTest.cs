cc9d266cf20db68b32bc3e025bd59fa7adabf18f
Hrishi <singhchauhanhrishiraaj@gmail.com>
2022-11-03 22:52:07 +0530

    running persistence

--- src/PlexShareTests/DashboardTests/Persistence/PersistenceUnitTest.cs
+++ src/PlexShareTests/DashboardTests/Persistence/PersistenceUnitTest.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using Xunit;
+using PlexShareDashboard.Server.Persistence;
 namespace PlexShareTests.DashboardTests.Persistence
 {
     public class PersistenceUnitTest
     {
         public PersistenceUnitTest()
         {
+            summary = "Hello succesfully saved the summary";
+            bool save = PersistenceFactory.GetSummaryPersistenceInstance().SaveSummary(summary, true);
+
         }
     }
 }
