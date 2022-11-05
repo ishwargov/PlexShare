5f1d211fce2746221a4ce27c0efcaa4f6f32d444
Jasir K <62549297+jasirkadarsha721091@users.noreply.github.com>
2022-11-05 18:40:33 +0530

    Updated the placeholder functions and bindings

--- src/PlexShareApp/HomePageView.xaml.cs
+++ src/PlexShareApp/HomePageView.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace PlexShareApp
 {
@@ -19,9 +20,29 @@ namespace PlexShareApp
     /// </summary>
     public partial class HomePageView : Window
     {
+        //string Name = "";
+        //string Email = "";
+        //string ImageLocation = "";
         public HomePageView()
         {
             InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer(new TimeSpan(0, 0, 1), DispatcherPriority.Normal, delegate
+            {
+                this.Time.Text = DateTime.Now.ToString("hh:mm:ss tt");
+                this.Date.Text = DateTime.Now.ToString("d MMMM yyyy, dddd");
+            }, this.Dispatcher);
+            //Name = name;
+            //Email = email;
+            //ImageLocation = imageLocation;
+        }
+
+        private void New_Meeting_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(this.Name_box.Text)){
+                this.Name_box.Text = "";
+                this.Name_block.Text = "Name!!!";
+                this.Name_block.Foreground = Brushes.Red;
+            }
         }
     }
 }
