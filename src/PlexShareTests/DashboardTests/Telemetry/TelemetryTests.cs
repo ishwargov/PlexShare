df460031e8e340f1b6692b07396c1c391f8d5911
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-11-05 21:47:56 +0530

    Added the telemetry tests class to write the unit testing for the telemetry submodule

---
+++ src/PlexShareTests/DashboardTests/Telemetry/TelemetryTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlexShareTests.DashboardTests.Telemetry
+{
+    public class TelemetryTests
+    {
+        //in  this we write the unit test for the telemetry submodule 
+
+    }
+}
