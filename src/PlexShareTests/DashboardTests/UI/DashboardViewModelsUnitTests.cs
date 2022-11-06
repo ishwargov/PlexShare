d9a7f89084a7be6ed3727e603ec90b23fe1dedb1
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-11-06 13:01:18 +0530

    Added the new folder UI to test the dashboard view model for this purpose

---
+++ src/PlexShareTests/DashboardTests/UI/DashboardViewModelsUnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlexShareTests.DashboardTests.UI
+{
+    public class DashboardViewModelsUnitTests
+    {
+        //here we will write the unit testing for the dashboard view model 
+
+    }
+}
