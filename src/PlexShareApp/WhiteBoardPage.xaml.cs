151200bd058de1a0efde81b1bf3f1212bc19bc81
Jerry John Thomas <jerryjohnthomastvm@gmail.com>
2022-11-06 19:01:03 +0530

    whiteboard MVVM refined

--- src/PlexShareApp/WhiteBoardPage.xaml.cs
+++ src/PlexShareApp/WhiteBoardPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using PlexShareWhiteboard;
 
 namespace PlexShareApp
 {
@@ -20,9 +22,96 @@ namespace PlexShareApp
     /// </summary>
     public partial class WhiteBoardPage : Page
     {
+        WhiteBoardViewModel viewModel;
+
         public WhiteBoardPage()
         {
             InitializeComponent();
+            viewModel = new WhiteBoardViewModel();
+            viewModel.ShapeItems = new ObservableCollection<ShapeItem>();
+            this.DataContext = viewModel;
         }
+
+        private void CanvasMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            var a = e.GetPosition(sender as Canvas);
+            viewModel.ShapeStart(a);
+        }
+
+        private void CanvasMouseMove(object sender, MouseEventArgs e)
+        {
+            var a = e.GetPosition(sender as Canvas);
+            viewModel.ShapeBuilding(a);
+        }
+
+        private void CanvasMouseUp(object sender, MouseEventArgs e)
+        {
+            var a = e.GetPosition(sender as Canvas);
+            viewModel.ShapeFinished(a);
+        }
+
+        private void RectangleCreateMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("create_rectangle");
+        }
+
+        private void CircleCreateMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("create_circle");
+
+        }
+        private void EllipseCreateMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("create_ellipse");
+        }
+
+        //Freehand_create_mode
+        private void FreehandCreateMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("create_freehand");
+
+        }
+        private void ColorGreen(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeFillBrush(Brushes.Green);
+        }
+
+        private void ColorRed(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeFillBrush(Brushes.Red);
+        }
+
+        private void ColorYellow(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeFillBrush(Brushes.Yellow);
+        }
+
+        private void IncreaseZIndex(object sender, RoutedEventArgs e)
+        {
+            viewModel.IncreaseZIndex();
+        }
+
+        private void DecreaseZIndex(object sender, RoutedEventArgs e)
+        {
+            viewModel.DecreaseZIndex();
+
+        }
+
+        private void ViewMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("view_mode");
+
+        }
+
+        private void DeleteMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("delete_mode");
+        }
+
+        private void SelectMode(object sender, RoutedEventArgs e)
+        {
+            viewModel.ChangeMode("select_mode");
+        }
+
     }
 }
---
+++ src/PlexShareWhiteboard/SelectObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace PlexShareWhiteboard
+{
+    internal class SelectObject
+    {
+        public bool ifSelected = false;
+        public ShapeItem selectedObject;
+        public Point InitialSelectionPoint;
+
+        public SelectObject()
+        {
+            ifSelected = false;
+        }
+    }
+}
