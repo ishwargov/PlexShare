eff1a250060a608915d991380aabfd07d717c36b
B Sai Subrahmanyam <111901013@smail.iitpkd.ac.in>
2022-11-08 18:17:44 +0530

    MVVM for the Submissions Page

---
+++ src/PlexShareCloudUX/SubmissionsModel.cs
@@ -0,0 +1,30 @@
+﻿using PlexShareCloud;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlexShareCloudUX
+{
+    internal class SubmissionsModel
+    {
+        public SubmissionsModel()
+        {
+
+        }
+
+        public List<SubmissionEntity>? SubmissionsList;
+
+        public List<SubmissionEntity> GetSubmissions(string sessionId, string userName)
+        {
+            SubmissionsList = new List<SubmissionEntity>();
+            return SubmissionsList;
+        }
+        public void DownloadPdf(int num)
+        {
+            //string path = "";
+            //File.WriteAllBytes(path, SubmissionsList[i].Pdf);
+        }
+    }
+}
---
+++ src/PlexShareCloudUX/SubmissionsViewModel.cs
@@ -0,0 +1,90 @@
+﻿using PlexShareCloud;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows;
+
+namespace PlexShareCloudUX
+{
+    public class SubmissionsViewModel :
+        INotifyPropertyChanged // Notifies clients that a property value has changed.
+    {
+        /// <summary>
+        /// Creates an instance of the Submissions ViewModel.
+        /// Gets the details of the submissions of the session conducted by the user.
+        /// Then dispatch the changes to the view.
+        /// <param name="sessionId">Id of the session for which we want the submissions.</param>
+        /// <param name="userName">The username of the user.</param>
+        /// </summary>
+        public SubmissionsViewModel(string sessionId, string userName)
+        {
+            _model = new SubmissionsModel();
+            List<SubmissionEntity> submissionsList = _model.GetSubmissions(sessionId, userName);
+            _ = this.ApplicationMainThreadDispatcher.BeginInvoke(
+                        DispatcherPriority.Normal,
+                        new Action<List<SubmissionEntity>>((submissionsList) =>
+                        {
+                            lock (this)
+                            {
+                                // Note that Bitmap cannot be automatically marshalled to the main thread
+                                // if it were created on the worker thread. Hence the data model just passes
+                                // the path to the image, and the main thread creates an image from it.
+
+                                this.ReceivedSubmissions = submissionsList;
+
+                                this.OnPropertyChanged("ReceivedSubmissions");
+                            }
+                        }),
+                        submissionsList);
+        }
+
+        /// <summary>
+        /// To store which pdf to download.
+        /// </summary>
+        public int SubmissionToDownload
+        {
+            set => _model.DownloadPdf(value);
+        }
+
+        /// <summary>
+        /// The received submissions.
+        /// </summary>
+        public List<SubmissionEntity>? ReceivedSubmissions
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// Property changed event raised when a property is changed on a component.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Handles the property changed event raised on a component.
+        /// </summary>
+        /// <param name="property">The name of the property.</param>
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        /// <summary>
+        /// Gets the dispatcher to the main thread. In case it is not available
+        /// (such as during unit testing) the dispatcher associated with the
+        /// current thread is returned.
+        /// </summary>
+        private Dispatcher ApplicationMainThreadDispatcher =>
+            (Application.Current?.Dispatcher != null) ?
+                    Application.Current.Dispatcher :
+                    Dispatcher.CurrentDispatcher;
+
+        /// <summary>
+        /// Underlying data model.
+        /// </summary>
+        private SubmissionsModel _model;
+    }
+}
