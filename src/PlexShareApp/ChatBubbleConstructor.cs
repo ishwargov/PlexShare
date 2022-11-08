5a1e4a8b57945b58e498e492fc83010659ad1526
S, Sughandhan (T DAI ADM-IN) <sughandhan.s@siemens.com>
2022-11-08 13:40:28 +0530

    Created ChatBubbleConstructor

---
+++ src/PlexShareApp/ChatBubbleConstructor.cs
@@ -0,0 +1,16 @@
+﻿/// <author> Sughandhan S </author>
+/// <created> 08/11/2022 </created>
+/// <summary>
+///     The following file helps in constructing the chat bubble based on whether the message
+///     is a chat or file
+/// </summary>
+
+using System;
+using System.Text;
+
+namespace PlexShareApp
+{
+    internal class ChatBubbleConstructor
+    {
+    }
+}
