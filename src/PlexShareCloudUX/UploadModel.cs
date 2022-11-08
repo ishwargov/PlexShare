a482ec4861e223bc9e72cde23e7755af114fdfc2
B Sai Subrahmanyam <111901013@smail.iitpkd.ac.in>
2022-11-08 18:18:06 +0530

    MVVM for the Uplaod Page

---
+++ src/PlexShareCloudUX/UploadModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlexShareCloudUX
+{
+    internal class UploadModel
+    {
+        public string SessionId;
+        public string UserName;
+        public UploadModel(string sessionId, string userName)
+        {
+            SessionId = sessionId;
+            UserName = userName;
+        }
+        public bool UploadDocument(string fileName)
+        {
+            //Upload File
+            /*System.IO.StreamReader sr = new System.IO.StreamReader(fileDialog.FileName);
+            MessageBox.Show(sr.ReadToEnd());
+            sr.Close();*/
+            return false;
+        }
+    }
+}
