23fb5a7ce70b3ad5ac268b1a286f9e7da24b4d38
Satyam Mishra <satyammishra37953@gmail.com>
2022-11-09 10:39:06 +0530

    ScreenProcessor test cases added

---
+++ src/PlexShareTests/ScreenshareTests/ScreenProcessorTests.cs
@@ -0,0 +1,44 @@
+﻿using PlexShareScreenshare.Client;
+using PlexShareTests.ScreenshareTests;
+using System.Diagnostics;
+using System.Drawing;
+
+ScreenProcessorTests tmp = new();
+tmp.Test1();
+
+namespace PlexShareTests.ScreenshareTests
+{
+    public class ScreenProcessorTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            ScreenCapturer screenCapturer = new();
+            ScreenProcessor screenProcessor = new(screenCapturer);
+
+            screenProcessor.StartProcessing();
+            screenCapturer.StartCapture();
+
+            Thread.Sleep(10000);
+
+            screenCapturer.StopCapture();
+            int v2 = screenProcessor.GetProcessedFrameLength();
+            screenProcessor.StopProcessing();
+
+            Assert.True(v2 > 0);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            ScreenCapturer screenCapturer = new();
+
+            screenCapturer.StartCapture();
+            Bitmap img = screenCapturer.GetImage();
+            screenCapturer.StopCapture();
+
+            List<Pixel> tmp = ScreenProcessor.ProcessUsingLockbits(img, img);
+            Assert.True(tmp.Count == 0);
+        }
+    }
+}
