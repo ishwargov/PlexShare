9fca87775ef25d474202268a928fe12171d2047e
S, Sughandhan (T DAI ADM-IN) <sughandhan.s@siemens.com>
2022-11-09 10:27:25 +0530

    added view model

--- src/PlexShareApp/ChatPageView.xaml.cs
+++ src/PlexShareApp/ChatPageView.xaml.cs
@@ -7,6 +7,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +20,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 using PlexShareApp.ViewModel;
 
 namespace PlexShareApp
@@ -49,14 +51,68 @@ namespace PlexShareApp
             
         }
 
+        /// <summary>
+        /// Replied message's Message ID 
+        /// </summary>
+        public int ReplyMsgId { get; set; }
+
+        /// <summary>
+        /// Upa
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Listner(object sender, PropertyChangedEventArgs e)
+        {
+            var propertyName = e.PropertyName; ;
+            var viewModel = DataContext as ChatPageViewModel;
+
+            if(propertyName == "ReceivedMsg")
+            {
+                _allMessages.Add(viewModel.ReceivedMsg);
+            }
+            else if(propertyName == "ReceivedMsgs")
+            {
+                _allMessages.Add(viewModel.ReceivedMsg);
+            }
+        }
+
+
         private void UploadButtonClick(object sender, RoutedEventArgs e)
         {
+            var viewModel = DataContext as ChatPageViewModel;
 
+            // Create OpenFileDialog
+            var openFileDialog = new OpenFileDialog();
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            var result = openFileDialog.ShowDialog();
+
+            // Process open file diaalog box results
+            if(result == true)
+            {
+                if(string.IsNullOrEmpty(ReplyTextBox.Text))
+                {
+                    viewModel.SendFile(openFileDialog.FileName, -1);
+                }
+                else
+                {
+                    viewModel.SendFile(openFileDialog.FileName, ReplyMsgId);
+                }
+                ReplyTextBox.Text = "";
+            }
         }
 
         private void SendButtonClick(object sender, RoutedEventArgs e)
         {
 
         }
+
+        // TODO: Implement ReplyButtonClick event
+
+        // TODO: Implement StarButtonClick event
+
+        // TODO: Implement DownloadButtonClick event
+
+        // TODO: Implement UpdateScrollBar event
     }
 }
--- src/PlexShareApp/ViewModel/ChatPageViewModel.cs
+++ src/PlexShareApp/ViewModel/ChatPageViewModel.cs
@@ -1,18 +1,112 @@
 ﻿/// <author> Sughandhan S </author>
 /// <created> 03/11/2022 </created>
 /// <summary>
-///     The following is the ViewModel for our ChatPage.
+///     The following is the ViewModel for our ChatPageView.
 /// </summary>
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using PlexShareContent.DataModels;
+using System.Windows.Threading;
+using System.Windows
 
 namespace PlexShareApp.ViewModel
 {
-    internal class ChatPageViewModel
+    public class ChatPageViewModel : INotifyPropertyChanged, IContentListner, IClientSessionNotificaions
     {
+        /// <summary>
+        /// Client Content Data Model
+        /// </summary>
+        private readonly IContentClient _model;
+
+        /// <summary>
+        /// Dashboard UX Data Model
+        /// </summary>
+        private readonly IUXClientSessionManager _modelDb;
+
+        /// <summary>
+        /// Dictionary mapping User IDs to their User names
+        /// </summary>
+        public IDictionary<int, string>? Users;
+
+        /// <summary>
+        /// Dictionary mapping Message IDs to their corresponding Message String
+        /// </summary>
+        public IDictionary<int, string>? Messages;
+
+        /// <summary>
+        /// Dictionary mapping Messages IDs to their ThreadIds
+        /// </summary>
+        public IDictionary<int, int>? ThreadIds;
+
+
+        public ChatPageViewModel(bool testing = false)
+        {
+            Users = new Dictionary<int, string>();
+            Messages = new Dictionary<int, string>();
+            ThreadIds = new Dictionary<int, int>();
+
+            if(!testing)
+            {
+                // Getting Content Client model and subscribing to the content module
+                _model = ContentClientFactory.GetInstance();
+                _model.ClientSubscribe(this);
+
+                // TODO: Get data model from Dashboard module and subscribe to them
+            }
+            
+        }
+
+        /// <summary>
+        /// The current user id
+        /// </summary>
+        public static int UserId { get; private set; }
+
+        /// <summary>
+        /// Message to be sent
+        /// </summary>
+        public SendContentData MsgToSend { get; private set; }
+
+        /// <summary>
+        /// The received message
+        /// </summary>
+        public Message ReceivedMsg { get; private set; }
+
+        /// <summary>
+        /// True means testing mode
+        /// </summary>
+        public bool Testing { get; }
+
+
+        /// <summary>
+        ///     Gets the dispatcher to the main thread. In case it is not available
+        ///     (such as during unit testing) the dispatcher associated with the
+        ///     current thread is returned.
+        /// </summary>
+        private Dispatcher ApplicationMainThreadDispatcher =>
+            Application.Current?.Dispatcher != null ? Application.Current.Dispatcher : Dispatcher.CurrentDispatcher;
+
+        // TODO: COMPLETE CODE
+
+        /// <summary>
+        /// Whenever a property changes, a Property Changed event is raised
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        // TODO: COMPLETE CODE
+
+        /// <summary>
+        /// Handling the Property Changed event raised 
+        /// </summary>
+        /// <param name="property">The name of the property.</param>
+        public void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
