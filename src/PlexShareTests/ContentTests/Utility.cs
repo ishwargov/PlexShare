0f6a7d940087ce039cf596b3ea2649124761b029
Narvik <66057572+Narvik09@users.noreply.github.com>
2022-11-09 23:39:35 +0530

    Added util function for notification tests

--- src/PlexShareTests/ContentTests/Utility.cs
+++ src/PlexShareTests/ContentTests/Utility.cs
@@ -149,5 +149,55 @@ namespace PlexShareTests.ContentTests
             };
             return newContentData;
         }
+
+        /// <summary>
+        /// Checks if two ReceiveContentData objects are equal
+        /// </summary>
+        /// <param name="rcdata1">ReceiveContentData object</param>
+        /// <param name="rcdata2">ReceiveContentData objeect</param>
+        public void CheckReceiveContentData(ReceiveContentData rcdata1, ReceiveContentData rcdata2)
+        {
+            Assert.Equal(rcdata1.Type, rcdata2.Type);
+            Assert.Equal(rcdata1.Data, rcdata2.Data);
+            Assert.Equal(rcdata1.MessageID, rcdata2.MessageID);
+            Assert.Equal(rcdata1.ReceiverIDs, rcdata2.ReceiverIDs);
+            Assert.Equal(rcdata1.SenderID, rcdata2.SenderID);
+            Assert.Equal(rcdata1.ReplyMessageID, rcdata2.ReplyMessageID);
+            Assert.Equal(rcdata1.ReplyThreadID, rcdata2.ReplyThreadID);
+            Assert.Equal(rcdata1.SenderID, rcdata2.SenderID);
+            Assert.Equal(rcdata1.Starred, rcdata2.Starred);
+            Assert.Equal(rcdata1.Event, rcdata2.Event);
+        }
+
+        /// <summary>
+        /// Checks if two chat threads are similar
+        /// </summary>
+        /// <param name="thread1">ChatThread object</param>
+        /// <param name="thread2">ChatThread object</param>
+        public void CheckChatThreads(ChatThread thread1, ChatThread thread2)
+        {
+            Assert.Equal(thread1.ThreadID, thread2.ThreadID);
+            Assert.Equal(thread1.MessageList.Count, thread2.MessageList.Count);
+            for(int i = 0; i < thread1.MessageList.Count; i++)
+            {
+                CheckReceiveContentData(thread1.MessageList[i], thread2.MessageList[i]);
+            }
+        }
+
+        /// <summary>
+        /// Checks if two lists of chat threads are similar
+        /// </summary>
+        /// <param name="list1">List of chat threads</param>
+        /// <param name="list2">List of chat threads</param>
+        public void CheckChatThreadLists(List<ChatThread> list1, List<ChatThread> list2)
+        {
+            Assert.Equal(list1.Count, list2.Count);
+            for(int i = 0; i < list1.Count; i++)
+            {
+                CheckChatThreads(list1[i], list2[i]);
+            }
+        }
     }
+
+    
 }
