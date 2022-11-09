0e40516e2766babcdc00f7a268332da1d158cfd5
Sughandhan S <sughandhan@yahoo.com>
2022-11-09 16:58:35 +0530

    Subscribe to PropertyChangedEvent and bind messages

--- src/PlexShareApp/ChatPageView.xaml.cs
+++ src/PlexShareApp/ChatPageView.xaml.cs
@@ -5,21 +5,12 @@
 /// </summary>
 
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using Microsoft.Win32;
 using PlexShareApp.ViewModel;
 
@@ -42,12 +33,13 @@ namespace PlexShareApp
 
             var viewModel = new ChatPageViewModel();
 
-            // TODO: Subscribe to the Property Changed Event
+            // Subscribed to the Property Changed Event
+            viewModel.PropertyChanged += Listener;
+            DataContext = viewModel;
 
-
+            // Binding all the messages
             _allMessages = new ObservableCollection<Message>();
-            
-            // TODO: Binding all the messages
+            MainChat.ItemsSource = _allMessages;
             
         }
 
@@ -57,20 +49,16 @@ namespace PlexShareApp
         public int ReplyMsgId { get; set; }
 
         /// <summary>
-        /// Upa
+        /// Property Changed Evenet in which the view gets updated with new messages
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Listner(object sender, PropertyChangedEventArgs e)
+        /// <param name="sender"> Sender Notifying the event </param>
+        /// <param name="e"> Property Changed Event </param>
+        private void Listener(object sender, PropertyChangedEventArgs e)
         {
             var propertyName = e.PropertyName; ;
             var viewModel = DataContext as ChatPageViewModel;
 
-            if(propertyName == "ReceivedMsg")
-            {
-                _allMessages.Add(viewModel.ReceivedMsg);
-            }
-            else if(propertyName == "ReceivedMsgs")
+            if(propertyName == "ReceivedMsg" || propertyName == "ReceivedMsgs")
             {
                 _allMessages.Add(viewModel.ReceivedMsg);
             }
@@ -87,7 +75,7 @@ namespace PlexShareApp
             // Launch OpenFileDialog by calling ShowDialog method
             var result = openFileDialog.ShowDialog();
 
-            // Process open file diaalog box results
+            // Process open file dialog box results
             if(result == true)
             {
                 if(string.IsNullOrEmpty(ReplyTextBox.Text))
