84e462ea99557f1edaf1e6c6608f367f6dbb1d35
Sughandhan S <sughandhan@yahoo.com>
2022-11-11 03:30:06 +0530

    Created ChatUtility.cs

---
+++ src/PlexShareTests/ContentTests/UX/ChatUtility.cs
@@ -0,0 +1,37 @@
+﻿/// <author>Sughandhan S</author>
+/// <created>11/11/2022</created>
+/// <summary>
+///     Unit Testing file for Utility Funtions
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace PlexShareTests.ContentTests.UX
+{
+    public class ChatUtility
+    {
+        public static class DispatcherUtil
+        {
+
+            private static object ExitFrame(object frame)
+            {
+                ((DispatcherFrame)frame).Continue = false;
+                return null;
+            }
+
+            public static void DoEvent()
+            {
+                var frame = new DispatcherFrame();
+                Dispatcher.CurrentDispatcher.BeginInvoke(
+                    DispatcherPriority.Background,
+                    new DispatcherOperationCallback(ExitFrame), frame);
+                Dispatcher.PushFrame(frame);
+            }
+        }
+    }
+}
