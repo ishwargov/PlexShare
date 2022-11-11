376e7114ee2809af82749bfa901f81f0d71d6ecb
dkodar20 <rudrtiwari@gmail.com>
2022-11-11 05:30:47 +0530

    Fix ScreenStitcher.cs

--- src/PlexShareScreenshare/Server/ScreenStitcher.cs
+++ src/PlexShareScreenshare/Server/ScreenStitcher.cs
@@ -4,53 +4,35 @@
 ///screen stitching functionality. It is used by ScreenshareServer. 
 ///</summary>
 
-using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
-using System.Windows.Controls;
-using static System.Net.Mime.MediaTypeNames;
 
 
-// Each frame consists of the resolution of the image and the ImageDiffList
-using Frame = System.Tuple<System.Tuple<int, int>,
-                        System.Collections.Generic.List<System.Tuple<System.Tuple<int, int>,
-                        System.Tuple<int, int, int>>>>;
-
-
-namespace ScreenShare.Server
+namespace PlexShareScreenshare.Server
 {
     /// <summary>
     /// Class contains implementation of the screen stitching using threads (tasks)
     /// </summary>
     internal class ScreenStitcher
     {
-        private SharedClientScreen _sharedClientScreen;
+        private readonly SharedClientScreen _sharedClientScreen;
 
-        private Task _stitchTask;
-        private Bitmap _stichedImage;
+        private Task? _stitchTask;
 
-        // new frame to hold the updated pixels
-        private Frame _newFrame;
-        private Bitmap _oldImage;
-        private Tuple<int, int> _resolution;
+        private Bitmap? _oldImage;
+        private Resolution? _resolution;
 
-        // Token and its source for killing the task
-        private CancellationTokenSource Source;
-        private CancellationToken Token;
+        // Token for killing the task
+        private bool _stitcherCancellationToken;
 
         // Called by the `SharedClientScreen`
         public ScreenStitcher(SharedClientScreen scs)
         {
-            _stichedImage = null;
-            _newFrame = null;
             _oldImage = null;
             _stitchTask = null;
             _resolution = null;
-            _sharedClientScreen= scs;
+            _sharedClientScreen = scs;
         }
         /// <summary>
         /// Creates(if not exist) and start the task `_stitchTask`
@@ -59,21 +41,26 @@ namespace ScreenShare.Server
         /// </summary>
         public void StartStitching()
         {
-            Source = new CancellationTokenSource();
-            Token = Source.Token;
+            _stitcherCancellationToken = false;
 
             if (_stitchTask == null)
             {
-                
-                    _stitchTask = new Task(() =>
+
+                _stitchTask = new Task(() =>
+                {
+                    while (!_stitcherCancellationToken)
                     {
-                        while (!Source.IsCancellationRequested)
+                        Frame? newFrame = _sharedClientScreen.GetImage();
+                        if (newFrame == null)
                         {
-                            _newFrame = _sharedClientScreen.GetImage();
-                            _stichedImage = Stitch(_oldImage, _newFrame);
-                            _sharedClientScreen.PutFinalImage(_stichedImage);
+                            Trace.WriteLine($"[ScreenSharing] New frame returned by _sharedClientScreen is null.");
+                            continue;
                         }
-                    }, Token);
+                        Bitmap stichedImage = Stitch(_oldImage, (Frame)newFrame);
+                        _oldImage = stichedImage;
+                        _sharedClientScreen.PutFinalImage(stichedImage);
+                    }
+                });
             }
 
             _stitchTask.Start();
@@ -83,9 +70,8 @@ namespace ScreenShare.Server
         // Kills the task `_stitchTask`
         public void StopStitching()
         {
-            Source.Cancel();
-            _stitchTask.Wait();
-            _stichedImage = null;
+            _stitcherCancellationToken = true;
+            _stitchTask?.Wait();
             _stitchTask = null;
         }
 
@@ -94,32 +80,29 @@ namespace ScreenShare.Server
         /// If resolution is changed update whole Image, else update the changed pixels
         /// </summary>
         /// <param name="oldImage">The previous image of client's screen</param>
-        /// <param name="frame">The list of the updated pixels, list of : { x, y, (R, G, B)}</param>
+        /// <param name="newFrame">The list of the updated pixels, list of : { x, y, (R, G, B)}</param>
         /// <returns>The updated new image of client's screen</returns>
-
-        private Bitmap Stitch(Bitmap oldImage, Frame frame)
+        private Bitmap Stitch(Bitmap? oldImage, Frame newFrame)
         {
-            Tuple<int, int> _newResolution = frame.Item1;
+            Resolution newResolution = newFrame.Resolution;
 
-            if (_newResolution != _resolution)
+            if (oldImage == null || newResolution != _resolution)
             {
-                oldImage = new Bitmap(_newResolution.Item1, _newResolution.Item2);
+                oldImage = new Bitmap(newResolution.Width, newResolution.Height);
             }
 
-            for (int x = 0; x < frame.Item2.Count; x++)
+            foreach (var pixel in newFrame.Pixels)
             {
-             
-                int xCordinate = frame.Item2[x].Item1.Item1;
-                int yCordinate = frame.Item2[x].Item1.Item2;
-                int red = frame.Item2[x].Item2.Item1;
-                int green = frame.Item2[x].Item2.Item2;
-                int blue = frame.Item2[x].Item2.Item3;
+                int xCordinate = pixel.Coordinates.X;
+                int yCordinate = pixel.Coordinates.Y;
+                int red = pixel.RGB.R;
+                int green = pixel.RGB.G;
+                int blue = pixel.RGB.B;
                 Color newColor = Color.FromArgb(red, green, blue);
-
                 oldImage.SetPixel(xCordinate, yCordinate, newColor);
             }
 
-            _resolution= _newResolution;
+            _resolution = newResolution;
             return oldImage;
         }
     }
