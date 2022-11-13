0628015be67798baaf19d96a6527ae01a5a43421
Saurabh <saurabhkumar121201@gmail.com>
2022-11-13 22:57:14 +0530

    corrected namespace

--- src/PlexShareDashboard/Dashboard/ClientToServerData.cs
+++ src/PlexShareDashboard/Dashboard/ClientToServerData.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PlexShare.Dashboard
+namespace PlexShareDashboard.Dashboard
 {
     public class ClientToServerData
     {
