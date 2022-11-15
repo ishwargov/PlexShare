1fd8a4af8b73971bcbf446bd3dca98959cb520ee
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2022-11-15 16:58:05 +0530

    Added some borders and margins to the graphs

--- src/PlexShareApp/DashboardPage.xaml.cs
+++ src/PlexShareApp/DashboardPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using PlexShareDashboard.Dashboard.UI.ViewModel;
+﻿using Dashboard;
+using MaterialDesignThemes.Wpf;
+using PlexShare.Dashboard;
+using PlexShareDashboard.Dashboard.Client.SessionManagement;
+using PlexShareDashboard.Dashboard.UI.ViewModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -33,10 +37,20 @@ namespace PlexShareApp
             //this.DataContext = 
             //here i can initialise the dashboard view model to be able to use 
             //and to be able to utilise the functionality to update the vm whenever the refresh button is clicked for this purpose 
+            ClientSessionManager clientSessionManager = SessionManagerFactory.GetClientSessionManager();
+            SessionData sessionData = clientSessionManager.GetSessionData();
+            string sessionId = sessionData.sessionId.ToString();
+            UserData currUser = clientSessionManager.GetUser();
+
+            string emailId = currUser.userEmail;
+
+            UploadPage FileUploadPage = new UploadPage(sessionId, emailId);
+            //Main.Content = FileUploadPage;
 
             this.DashboardViewModelInstance = new DashboardViewModel();
             this.DataContext = DashboardViewModelInstance;
-                    }
+            //NavigationPage MainPage = new NavigationPage(new DashboardPage());
+        }
 
         //private void InitializeComponent()
         //{
@@ -77,7 +91,16 @@ namespace PlexShareApp
             //the file will be directly send to the viewmodel of the cloud team 
             //i have to call the view of the cloud UI to give option to upload the file
             //the file will be directly send to the viewmodel of the cloud team 
+            //we have to get the user and the email id of the user as the username for this purpose 
 
+            //NewXamlUserControl ui = new NewXamlUserControl();
+            //DashboardPage newWindow = new DashboardPage();
+            //newWindow.Content = FileUploadPage;
+            //newWindow.Show();
+            //FileUploadPage.Show();
+            //FileUploadPage.ShowDialog);
+            //Main.IsEnabled = true;
+            return;
         }
     }
 }
