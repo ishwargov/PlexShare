d95dfb150536f4ae5e3f07290a2e836cfb44aee6
Neel Kabra <kabraneel@gmail.com>
2022-11-18 00:02:25 +0530

    Added random comments

--- src/PlexShareApp/MainWindow.xaml.cs
+++ src/PlexShareApp/MainWindow.xaml.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/******************************************************************************
+ * Filename    = MainWindow.xaml.cs
+ *
+ * Author      = Neel Kabra
+ *
+ * Product     = PlexShare
+ * 
+ * Project     = PlexShareApp
+ *
+ * Description = This is start view of the application. It is responsible for starting the authenticationView.
+ * 
+ *****************************************************************************/
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
 namespace PlexShareApp
 {
@@ -24,7 +22,7 @@ namespace PlexShareApp
         public MainWindow()
         {
             InitializeComponent();
-            
+
             /*
              *  Main Code :
              */
@@ -36,7 +34,7 @@ namespace PlexShareApp
             /*
              *  If everyone does not like to go through authentication while testing
              *  Comment the above lines and uncomment the below lines
-             */ 
+             */
 
             //HomePageView Homeview = new HomePageView("Neel","111901057@smail.iitpkd.ac.in", "https://images.unsplash.com/photo-1661956602868-6ae368943878?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80");
             //Homeview.Show();
