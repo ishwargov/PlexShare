3f194b949a102bcb5d033cf5f4bab8c2f364c6e6
Polisetty Vamsi <111901041@smail.iitpkd.ac.in>
2022-11-17 12:21:05 +0530

    Added test case for testing the file read functionality.

---
+++ src/PlexShareTests/CloudTests/TestreadPathFromFile.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using PlexShareCloud;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PlexShareCloudUX;
+
+namespace PlexShareTests.CloudTests
+{
+    public class TestreadPathFromFile
+    {
+        [Fact]
+        public void GetPath()
+        {
+            // Create an entity.
+            //Logger.LogMessage("Create an entity.");
+            /*
+            Assert.Equal(1, getEntity?.Count);
+            for (int i = 0; i < getEntity?.Count; i++)
+            {
+                Assert.Equal(postEntity?.SessionId, getEntity[i].SessionId);
+            }*/
+            //FileRead fileRead = new FileRead;
+            string[] lines = FileRead.GetPaths();
+            Assert.NotNull(lines);
+            //Assert.Equal(postEntity?.Name, getEntity?.Name);
+        }
+    }
+}
