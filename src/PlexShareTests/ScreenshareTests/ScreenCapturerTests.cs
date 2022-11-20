1fd87e9f6dd13ff4af45d5ae6203432557e914b1
Amish Saxena <111901008@smail.iitpkd.ac.in>
2022-11-21 02:52:43 +0530

    Add test names & commnets

--- src/PlexShareTests/ScreenshareTests/ScreenCapturerTests.cs
+++ src/PlexShareTests/ScreenshareTests/ScreenCapturerTests.cs
@@ -1,4 +1,9 @@
-﻿using PlexShareScreenshare.Client;
+﻿///<author>Amish Ashish Saxena</author>
+///<summary> 
+///This file contains tests for ScreenCapturer Class.  
+///</summary>
+
+using PlexShareScreenshare.Client;
 using System.Drawing;
 
 namespace PlexShareTests.ScreenshareTests
@@ -7,16 +12,17 @@ namespace PlexShareTests.ScreenshareTests
     public class ScreenCapturerTests
     {
         /// <summary>
-        /// Capture for some time and see if the captured images inside the queue are not null
+        /// Capture for some time and see if the captured images inside the queue are not null.
         /// </summary>
         [Fact]
-        public void Test1()
+        public void CorrectImageCaptureTest()
         {
             Task<ScreenCapturer> task = Task.Run(() => { ScreenCapturer screenCapturer = new ScreenCapturer(); return screenCapturer; });
             task.Wait();
             var screenCapturer = task.Result;
             screenCapturer.StartCapture();
             Thread.Sleep(1000);
+
             int count = 0;
             bool token = false;
             for (int i = 0; i < 50; i++)
@@ -31,21 +37,46 @@ namespace PlexShareTests.ScreenshareTests
         }
 
         /// <summary>
-        /// Queue length should always be between 0 and MaxQueueLength
+        /// Queue length should always be between 0 and MaxQueueLength.
         /// </summary>
         [Fact]
-        public void Test2()
+        public void CapturedFrameQueueFunctionality()
         {
             Task<ScreenCapturer> task = Task.Run(() => { ScreenCapturer screenCapturer = new ScreenCapturer(); return screenCapturer; });
             task.Wait();
             var screenCapturer = task.Result;
+
             screenCapturer.StartCapture();
             Thread.Sleep(1000);
             int framesCaptured = screenCapturer.GetCapturedFrameLength();
-
             screenCapturer.StopCapture();
-            Thread.Sleep(1);
+
             Assert.True(framesCaptured is > 0 and <= ScreenCapturer.MaxQueueLength);
+        }
+
+        /// <summary>
+        /// Test the control flow where screen ccapturing is stopped once and then restarted and stopped.
+        /// This is to simulate a real-life scenario.
+        /// </summary>
+        [Fact]
+        public void MultipleStartStopCapturer()
+        {
+            Task<ScreenCapturer> task = Task.Run(() => { ScreenCapturer screenCapturer = new ScreenCapturer(); return screenCapturer; });
+            task.Wait();
+            var screenCapturer = task.Result;
+
+            screenCapturer.StartCapture();
+            Thread.Sleep(500);
+            screenCapturer.StopCapture();
+            Thread.Sleep(100);
+
+            screenCapturer.StartCapture();
+            Thread.Sleep(500);
+            screenCapturer.StopCapture();
+
+            // If the queue is empty, this means the capturing has been stopped sucessfully. 
+            int framesCaptured = screenCapturer.GetCapturedFrameLength();
+            Assert.True(framesCaptured == 0);
         }
     }
 }
